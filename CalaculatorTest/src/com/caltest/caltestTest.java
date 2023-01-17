482b44f63f4f377d2c1b3af68774559c15a77bd8
somya vishnoi <somya.vishnoi012@gmail.com>
2023-01-17 18:16:52 +0530

    commit all

---
+++ CalaculatorTest/src/com/caltest/caltestTest.java
@@ -0,0 +1,29 @@
+package com.caltest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+
+import com.caltest.*;
+class caltestTest {
+
+	@Test
+	public void caldemo() {
+		assertEquals(30, caltest.add(10, 20));
+	}
+	
+	@Test
+	public void caldemo() {
+		assertEquals(30, caltest.add(10, 20));
+	}
+	@Test
+	public void caltest() {
+		assertEquals(30, caltest.add(10, 20));
+	}
+	@Test
+	public void caltest() {
+		assertEquals(30, caltest.add(10, 20));
+	}
+
+}
---
+++ Dynamicwebproject1/src/main/java/com/webclass/webclass1.java
@@ -0,0 +1,13 @@
+package com.webclass;
+
+public class webclass1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("java dynamic project");
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
